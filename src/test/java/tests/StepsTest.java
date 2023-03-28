43b219f779deb405508608bb9419edc4e6563f51
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-03-28 22:45:53 +0300

    added steps test

---
+++ src/test/java/tests/StepsTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 81;
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Open Main Page", () -> {
+            open("https://github.com");
+        });
+
+
+        step("Find and Click Repository" + REPOSITORY, () -> {
+            $(".header-search-input").setValue(REPOSITORY).submit();
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("open tab issues", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("check if Issue number avaliable" + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+
+
+    }
+}
