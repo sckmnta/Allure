e48ab6d6b8ed97b3a31e5f33bfad8d4ec0d8ddec
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-03-29 18:35:04 +0300

    added steps test

---
+++ src/test/java/tests/AttachmentsTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class AttachmentsTest {
+
+
+    @Test
+    public void testLambdaAttachmentsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Open Main Page", () -> {
+            open("https://github.com");
+            attachment("Source",webdriver().driver().source());
+        });
+
+
+
+    }
+    @Test
+    public void annotatedStepTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.takeScreenshot();
+    }
+
+}
---
+++ src/test/java/tests/LabelsTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LabelsTest {
+    @Test
+    @Feature("Issue in repository")
+    @Story("Creation of Issue in repository")
+    @Owner("sckmnta")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://testing.github.com")
+    @DisplayName("Creation of Issue for authorised user")
+    public void staticLabelsTest() {
+
+    }
+    @Test
+    public void dynamicLabelsTest() {
+        Allure.getLifecycle().updateTestCase(
+                t -> t.setName("Creation of Issue for authorised user"));
+                Allure.feature("Issue in repository");
+                Allure.story("Creation of Issue in repository");
+                Allure.label("Owner", "sckmnta");
+        Allure.label("Severity", SeverityLevel.BLOCKER.value());
+        Allure.link("Testing", "https://testing.github.com");
+
+
+    }
+}
